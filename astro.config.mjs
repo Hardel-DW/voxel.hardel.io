0bea11bc3dc06adc8f39b3ce9ca1784cbb3935c9
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2024-03-20 08:34:51 +0100

    Added vercel adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://voxel.hardel.io/",
-    output: "server",
-    integrations: [tailwind(), db()]
+  site: "https://voxel.hardel.io/",
+  output: "server",
+  integrations: [tailwind(), db()],
+  adapter: vercel()
 });

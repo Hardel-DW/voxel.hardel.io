a0b32df907ae50291e597e0bfda115e567af4ffe
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2024-03-29 21:56:33 +0100

    Removed AstroDB Database

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import db from "@astrojs/db";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://voxel.hardel.io/",
-  output: "server",
-  integrations: [tailwind(), db()],
-  adapter: vercel()
+    site: "https://voxel.hardel.io/",
+    image: ['https://lh3.googleusercontent.com/'],
+    output: "server",
+    integrations: [tailwind(), react()],
+    adapter: vercel()
 });
--- db/seed.ts
+++
@@ -1,8 +0,0 @@
-import {db, WaitList} from 'astro:db';
-
-// https://astro.build/db/seed
-export default async function seed() {
-    await db.insert(WaitList).values({
-        email: "voxel.hardel@gmail.com"
-    })
-}

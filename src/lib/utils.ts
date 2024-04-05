93907343377070a6f9fa19abb8d725d6c4a0b06c
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2024-04-05 03:54:39 +0200

    Comment util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,40 +2,59 @@ import { twMerge } from "tailwind-merge";
 
 export type ClassDictionary = Record<string, any>;
 export type ClassArray = ClassValue[];
-export type ClassValue = ClassArray | ClassDictionary | string | number | null | boolean | undefined;
+export type ClassValue =
+  | ClassArray
+  | ClassDictionary
+  | string
+  | number
+  | null
+  | boolean
+  | undefined;
 
 function toVal(mix: ClassValue): string {
-    if (typeof mix === "string" || typeof mix === "number") {
-        return String(mix);
-    } else if (Array.isArray(mix)) {
-        return mix
-            .map((item: ClassValue) => toVal(item))
-            .filter(Boolean)
-            .join(" ");
-    } else if (typeof mix === "object" && mix !== null) {
-        return Object.keys(mix)
-            .filter((key) => mix[key])
-            .join(" ");
-    }
-    return "";
+  if (typeof mix === "string" || typeof mix === "number") {
+    return String(mix);
+  } else if (Array.isArray(mix)) {
+    return mix
+      .map((item: ClassValue) => toVal(item))
+      .filter(Boolean)
+      .join(" ");
+  } else if (typeof mix === "object" && mix !== null) {
+    return Object.keys(mix)
+      .filter((key) => mix[key])
+      .join(" ");
+  }
+  return "";
 }
 
+/**
+ * A utility function to merge classnames
+ * @param args
+ */
 function clsx(...args: ClassValue[]): string {
-    return args
-        .map((arg) => toVal(arg))
-        .filter(Boolean)
-        .join(" ");
+  return args
+    .map((arg) => toVal(arg))
+    .filter(Boolean)
+    .join(" ");
 }
 
 export function cn(...args: ClassValue[]) {
-    return twMerge(clsx(args));
+  return twMerge(clsx(args));
 }
 
-export const getFormattedDate = (date: string | number | Date, regions: string = "fr-fr") =>
-    date
-        ? new Date(date).toLocaleDateString(regions, {
-              year: "numeric",
-              month: "short",
-              day: "numeric"
-          })
-        : "";
+/**
+ * Get the formatted date in the format "dd MMM yyyy"
+ * @param date
+ * @param regions
+ */
+export const getFormattedDate = (
+  date: string | number | Date,
+  regions: string = "fr-fr"
+) =>
+  date
+    ? new Date(date).toLocaleDateString(regions, {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })
+    : "";

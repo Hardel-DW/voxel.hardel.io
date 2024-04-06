eb59a0c970a43f6a699581a7a3f8f833645d0ad0
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2024-04-06 19:03:53 +0200

    Reformat Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,39 +2,39 @@ import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./src/**/*.{astro,md,mdx,ts,tsx}"],
-  theme: {
-    screens: {
-      s: "420px",
-      ...defaultTheme.screens,
+    content: ["./src/**/*.{astro,md,mdx,ts,tsx}"],
+    theme: {
+        screens: {
+            s: "420px",
+            ...defaultTheme.screens
+        },
+        extend: {
+            fontFamily: {
+                sans: ["Inter"]
+            },
+            colors: {
+                header: {
+                    translucent: "#18181bcc",
+                    cloudy: "#151418"
+                },
+                content: "#38373f66"
+            },
+            animation: {
+                levitate: "levitate 5s ease-in-out infinite",
+                fadein: "fadein 0.2s ease-in"
+            },
+            keyframes: {
+                levitate: {
+                    "0%": { transform: "translateY(-1.5rem)" },
+                    "50%": { transform: "translateY(1.5rem)" },
+                    "100%": { transform: "translateY(-1.5rem)" }
+                },
+                fadein: {
+                    from: { opacity: "0" },
+                    to: { opacity: "1" }
+                }
+            }
+        }
     },
-    extend: {
-      fontFamily: {
-        sans: ["Inter"],
-      },
-      colors: {
-        header: {
-          translucent: "#18181bcc",
-          cloudy: "#151418",
-        },
-        content: "#38373f66",
-      },
-      animation: {
-        levitate: "levitate 5s ease-in-out infinite",
-        fadein: "fadein 0.2s ease-in",
-      },
-      keyframes: {
-        levitate: {
-          "0%": { transform: "translateY(-1.5rem)" },
-          "50%": { transform: "translateY(1.5rem)" },
-          "100%": { transform: "translateY(-1.5rem)" },
-        },
-        fadein: {
-          from: { opacity: "0" },
-          to: { opacity: "1" },
-        },
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
+    plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")]
 } satisfies Config;

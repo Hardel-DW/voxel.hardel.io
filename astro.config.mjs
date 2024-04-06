ee3044d7fbaf50180641d2c0e2c8147a6a964da7
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2024-04-06 23:06:26 +0200

    Test Fix Google Auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import {defineConfig} from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://voxel.hardel.io/",
+    site: "https://devvoxel.hardel.io/",
     image: {
-        domains: ["https://voxel.hardel.io/", "https://lh3.googleusercontent.com/"]
+        domains: ["https://voxel.hardel.io/", "https://devvoxel.hardel.io/", "https://lh3.googleusercontent.com/"]
     },
     output: "server",
     integrations: [tailwind(), react()],

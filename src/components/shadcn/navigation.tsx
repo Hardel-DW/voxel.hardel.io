f2a23b7dd3edba2d6d14199498e5516f41799120
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2024-04-16 05:47:21 +0200

    Reformat and fix check for pushing to prod

--- src/components/shadcn/navigation.tsx
+++ src/components/shadcn/navigation.tsx
@@ -3,20 +3,17 @@ import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import * as React from "react";
 
 const NavigationMenu = React.forwardRef<
-  React.ElementRef<typeof NavigationMenuPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root>
+    React.ElementRef<typeof NavigationMenuPrimitive.Root>,
+    React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root>
 >(({ className, children, ...props }, ref) => (
-  <NavigationMenuPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative z-10 flex max-w-max flex-1 items-center justify-center",
-      className
-    )}
-    {...props}
-  >
-    {children}
-    <NavigationMenuViewport />
-  </NavigationMenuPrimitive.Root>
+    <NavigationMenuPrimitive.Root
+        ref={ref}
+        className={cn("relative z-10 flex max-w-max flex-1 items-center justify-center", className)}
+        {...props}
+    >
+        {children}
+        <NavigationMenuViewport />
+    </NavigationMenuPrimitive.Root>
 ));
 NavigationMenu.displayName = NavigationMenuPrimitive.Root.displayName;
 
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,10 @@
 import { defineCollection, z } from "astro:content";
 
 const articleCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean().default(false),
-    author: z.string().default("Hardel"),
-  }),
+    schema: z.object({
+        draft: z.boolean().default(false),
+        author: z.string().default("Hardel")
+    })
 });
 
 const guideCollection = defineCollection({

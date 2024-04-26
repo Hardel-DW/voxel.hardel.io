f8720bfa3061bbe6e3bfaadd806ba977c5d17ea7
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2024-04-26 08:44:12 +0200

    check: removed unread import

--- src/components/pages/guide/content/Navigation.tsx
+++ src/components/pages/guide/content/Navigation.tsx
@@ -1,7 +1,6 @@
 import type { CollectionEntry } from "astro:content";
 import GuideTree from "@/components/pages/guide/content/GuideTree.tsx";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import React from "react";
 import NavigationTree from "./NavigationTree.tsx";
 
 export type Guides = {

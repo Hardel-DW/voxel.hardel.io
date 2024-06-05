914703d79f63164c1cf82dfca30004e5312feaf6
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2024-06-05 05:25:12 +0200

    Test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,50 +1,8 @@
 import { defineMiddleware } from "astro:middleware";
-import { lucia } from "@/lib/lucia.ts";
-import type { AstroCookieSetOptions } from "astro";
-import { verifyRequestOrigin } from "lucia";
 
 /**
  * These pages are pre-rendered and cause warnings when the middleware is applied to them.
  */
 export const onRequest = defineMiddleware(async (context, next) => {
-    const excludedPaths = ["/", "/guides", "/contact", "/terms", "/privacy", "/404"];
-    if (excludedPaths.includes(context.url.pathname)) {
-        return next();
-    }
-
-    if (context.url.pathname.startsWith("/api/payment/webhook")) {
-        return next();
-    }
-
-    if (context.request.method !== "GET") {
-        const originHeader = context.request.headers.get("Origin");
-        const hostHeader = context.request.headers.get("Host");
-        if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader])) {
-            return new Response(null, {
-                status: 403
-            });
-        }
-    }
-
-    const sessionId = context.cookies.get(lucia.sessionCookieName)?.value ?? null;
-    if (!sessionId) {
-        context.locals.user = null;
-        context.locals.session = null;
-        return next();
-    }
-
-    const { session, user } = await lucia.validateSession(sessionId);
-    if (session?.fresh) {
-        const sessionCookie = lucia.createSessionCookie(session.id);
-        context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes as AstroCookieSetOptions);
-    }
-
-    if (!session) {
-        const sessionCookie = lucia.createBlankSessionCookie();
-        context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes as AstroCookieSetOptions);
-    }
-
-    context.locals.session = session;
-    context.locals.user = user;
     return next();
 });

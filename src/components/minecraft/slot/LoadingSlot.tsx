75a87782a43137d0209f9b5a207e3dbb313572e3
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2024-11-22 10:31:25 +0100

    Update imports

--- src/components/minecraft/slot/LoadingSlot.tsx
+++ src/components/minecraft/slot/LoadingSlot.tsx
@@ -1,10 +1,8 @@
-import PlaceHolderItem from "@images/design/item_placeholder.webp";
-
 export default function LoadingSlot() {
     return (
         <span className={"border border-white/20 w-14 h-14 p-[4px] relative hover:bg-zinc-800"}>
             {Math.random() > 0.5 && (
-                <img src={PlaceHolderItem.src} alt="Skeleton" height={64} width={64} className="w-full h-full pixelated" />
+                <img src="/images/features/gui/arrow.webp" alt="Skeleton" height={64} width={64} className="w-full h-full pixelated" />
             )}
         </span>
     );

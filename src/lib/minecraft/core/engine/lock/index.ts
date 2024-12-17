81d1f0de4247bac7aacb44bbcc0bd81e2d88ee75
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2024-12-17 08:40:25 +0100

    Added Vanilla Datapack, Social Link and some description change

---
+++ src/lib/minecraft/core/engine/lock/index.ts
@@ -0,0 +1,21 @@
+import type { Lock } from "@/lib/minecraft/core/schema/primitive/component";
+import type { Analysers } from "@/lib/minecraft/core/engine/Analyser";
+import { checkCondition } from "@/lib/minecraft/core/engine/condition";
+import type { TranslateTextType } from "@/lib/minecraft/core/schema/primitive/text";
+import type { GetAnalyserVoxel } from "@/lib/minecraft/core/engine/Analyser";
+import type { RegistryElement } from "@/lib/minecraft/mczip";
+
+export function checkLocks<T extends keyof Analysers>(
+    locks: Lock[] | undefined,
+    element: RegistryElement<GetAnalyserVoxel<T>>
+): { isLocked: boolean; text?: TranslateTextType } {
+    if (!locks) return { isLocked: false };
+
+    for (const lock of locks) {
+        if (checkCondition<T>(lock.condition, element)) {
+            return { isLocked: true, text: lock.text };
+        }
+    }
+
+    return { isLocked: false };
+}

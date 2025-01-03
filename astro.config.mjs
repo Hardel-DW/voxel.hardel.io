d5ac9281fbdbbf6fa92b8fd2add7278d12157ca6
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2025-01-03 21:21:15 +0100

    Remove i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
@@ -20,10 +19,6 @@ export default defineConfig({
         domains: ["https://voxel.hardel.io/", "https://devvoxel.hardel.io/", "https://lh3.googleusercontent.com/"]
     },
     output: "server",
-    i18n: {
-        defaultLocale: "en-us",
-        locales: ["en-us", "fr-fr"]
-    },
     vite: {
         plugins: [tailwindcss()]
     },
